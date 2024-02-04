d5555f9adac509cb0566bca69df4d9497137cd5a
Lawrence <lawrence891106@gmail.com>
2024-02-04 17:06:14 +0800

    This is the first setting

---
+++ src/components/home-components.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomeComponent = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="container-fluid">
+          <p>這是我的自我介紹網站測試</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default HomeComponent;
